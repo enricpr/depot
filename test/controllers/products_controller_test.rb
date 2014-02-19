fb54306b441f77790c849808c96ba6d342baa85d
Enric <kikflip@gmail.com>
2014-02-19 17:58:22 +0100

    Shopping Cart

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -41,7 +41,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    patch :update, id: @product, product: @update }
+    patch :update, id: @product, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
 
--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,9 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+	assert_select '.posts .post', 3
+	assert_select 'h3', 'McFarlane NBA Figure #1 Allen Iverson'
+	assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
