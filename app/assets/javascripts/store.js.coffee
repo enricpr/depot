430eb90d2189d04cf7b520cbdad3a08d3960ca78
Enric <kikflip@gmail.com>
2014-02-21 17:06:48 +0100

    ajax

--- app/assets/javascripts/store.js.coffee
+++ app/assets/javascripts/store.js.coffee
@@ -1,3 +1,7 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on "ready page:change", ->
+  $('.post-content > img').click ->
+    $(this).closest(".post").find(':submit').click()
--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -8,5 +8,11 @@ class StoreControllerTest < ActionController::TestCase
 	assert_select 'h3', 'McFarlane NBA Figure #1 Allen Iverson'
 	assert_select '.price', /\$[,\d]+\.\d\d/
   end
+  
+  test "markup needed for store.js.coffee is in place" do
+    get :index
+	assert_select '.post-content >img', 3
+	assert_select '.entry input[type=submit]', 3
+  end
 
 end
